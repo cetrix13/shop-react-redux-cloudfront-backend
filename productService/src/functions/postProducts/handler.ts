2ffa170064040b0c6320c60e7895ab6cedaecc5c
Alexander Dadykin <alexander_dadykin@epam.com>
2022-08-27 08:36:49 +0200

    Add middly/http-cors

--- productService/src/functions/postProducts/handler.ts
+++ productService/src/functions/postProducts/handler.ts
@@ -4,29 +4,35 @@ import { Client } from 'pg';
 import QUERIES from 'src/constants/queries';
 
 export const createProduct = async ({ body }) => {
-  const { id, title, description, price, count, img } = JSON.parse(body);
-
-  const client = new Client({
-    host: process.env.PG_HOST,
-    user: process.env.PG_USERNAME,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  });
-
-  await client.connect();
-
   try {
-    await client.query(QUERIES.createPost(id, title, description, count, price, img));
+    const parsedBody = typeof body !== 'object' ? JSON.parse(body) : body;
+    const { id, title, description, price, count, img } = parsedBody;
+
+    const client = new Client({
+      host: process.env.PG_HOST,
+      user: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    });
+
+    await client.connect();
+
+    try {
+      await client.query(QUERIES.createPost(id, title, description, count, price, img));
+    } catch (err) {
+      console.log(err);
+      return formatJSONResponse('Server error', HttpCode.SERVER_ERROR);
+    } finally {
+      await client.end();
+    }
+
+    const newProduct = { id, title, description, price, count, img };
+
+    return formatJSONResponse(newProduct);
   } catch (err) {
-    console.log(err);
-    return formatJSONResponse('Server error', HttpCode.SERVER_ERROR);
-  } finally {
-    await client.end();
+    console.error(err);
+    return formatJSONResponse(err, HttpCode.SERVER_ERROR);
   }
-
-  const newProduct = { id, title, description, price, count, img };
-
-  return formatJSONResponse(newProduct);
 }
 
 export const main = middyfy(createProduct);
